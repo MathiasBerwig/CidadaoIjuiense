ff0d1c4374caf061d057a10c9829a1ca623f4d39
Mathias Berwig <mathiasberwig@gmail.com>
2016-10-11 17:32:26 -0300

    Título endereço da ocorrência adicionado

--- app/src/main/java/io/github/mathiasberwig/cidadao_ijuense/presentation/fragment/SelecionarLocalFragment.java
+++ app/src/main/java/io/github/mathiasberwig/cidadao_ijuense/presentation/fragment/SelecionarLocalFragment.java
@@ -1,11 +1,13 @@
 package io.github.mathiasberwig.cidadao_ijuense.presentation.fragment;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import butterknife.ButterKnife;
 import io.github.mathiasberwig.cidadao_ijuense.R;
 
 public class SelecionarLocalFragment extends Fragment {
@@ -21,7 +23,13 @@ public class SelecionarLocalFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_selecionar_local, container, false);
+        View view = inflater.inflate(R.layout.fragment_selecionar_local, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 }

66f01ce110f4510824f2115e2536a1657cc225f9
Mathias Berwig <mathiasberwig@gmail.com>
2016-10-12 11:50:30 -0300

    Botão Submeter Ocorrência + Dialog de confirmação Anônimo

--- app/src/main/java/io/github/mathiasberwig/cidadao_ijuense/presentation/fragment/SubmeterOcorrenciaFragment.java
+++ app/src/main/java/io/github/mathiasberwig/cidadao_ijuense/presentation/fragment/SubmeterOcorrenciaFragment.java
@@ -1,17 +1,22 @@
 package io.github.mathiasberwig.cidadao_ijuense.presentation.fragment;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnCheckedChanged;
+import butterknife.OnClick;
 import io.github.mathiasberwig.cidadao_ijuense.R;
 
 public class SubmeterOcorrenciaFragment extends Fragment {
@@ -25,6 +30,8 @@ public class SubmeterOcorrenciaFragment extends Fragment {
     @BindView(R.id.cb_receber_feedback) CheckBox cbReceberFeedback;
 
     private OnSubmeterOcorrenciaListener listener;
+
+    private boolean ignoreNextOnCheck;
 
     public static SubmeterOcorrenciaFragment newInstance() {
         return new SubmeterOcorrenciaFragment();
@@ -59,21 +66,65 @@ public class SubmeterOcorrenciaFragment extends Fragment {
     }
 
     @OnCheckedChanged(R.id.cb_ocorrencia_anonima)
-    void onOcorrenciaAnonimaChecked(boolean checked) {
-        inputNomeUsuario.setEnabled(!checked);
-        inputEmailUsuario.setEnabled(!checked);
-        inputTelefoneUsuario.setEnabled(!checked);
-        cbReceberFeedback.setEnabled(!checked);
+    void onOcorrenciaAnonimaChecked(final boolean checked) {
+        if (ignoreNextOnCheck) {
+            ignoreNextOnCheck = false;
+            return;
+        }
 
-        if (checked) {
-            inputNomeUsuario.setText(null);
-            inputEmailUsuario.setText(null);
-            inputTelefoneUsuario.setText(null);
-            cbReceberFeedback.setChecked(false);
+        new AlertDialog.Builder(getContext())
+                .setTitle(R.string.dialog_tit_confirmacao_anonimo)
+                .setMessage(R.string.dialog_mes_confirmacao_anonimo)
+                .setPositiveButton(R.string.action_sim, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        inputNomeUsuario.setEnabled(!checked);
+                        inputEmailUsuario.setEnabled(!checked);
+                        inputTelefoneUsuario.setEnabled(!checked);
+                        cbReceberFeedback.setEnabled(!checked);
+
+                        if (checked) {
+                            inputNomeUsuario.setText(null);
+                            inputEmailUsuario.setText(null);
+                            inputTelefoneUsuario.setText(null);
+                            cbReceberFeedback.setChecked(false);
+                        }
+                    }
+                })
+                .setNegativeButton(R.string.action_cancelar, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ignoreNextOnCheck = true;
+                        cbOcorrenciaAnonima.setChecked(!checked);
+                    }
+                })
+                .show();
+    }
+
+    @OnClick(R.id.btn_submeter_ocorrencia)
+    void submeterOcorrencia() {
+        if (verificarCampos()) {
+            listener.onOcorrenciaSubmetida(
+                    inputNomeUsuario.getText().toString(),
+                    inputTelefoneUsuario.getText().toString(),
+                    inputEmailUsuario.getText().toString(),
+                    cbReceberFeedback.isChecked());
+        } else {
+            Toast toast = Toast.makeText(getContext(), R.string.toast_preencha_corretamente, Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER, 0, 0);
+            toast.show();
         }
     }
 
+    private boolean verificarCampos() {
+        return cbOcorrenciaAnonima.isChecked() ||
+                inputNomeUsuario.getText().toString().trim().length() != 0 &&
+                inputTelefoneUsuario.getText().toString().trim().length() != 0 &&
+                inputEmailUsuario.getText().toString().trim().length() != 0;
+
+    }
+
     public interface OnSubmeterOcorrenciaListener {
-        void onOcorrenciaSubmetida(String nome, String telefone, boolean feedback);
+        void onOcorrenciaSubmetida(String nome, String telefone, String email, boolean feedback);
     }
 }
